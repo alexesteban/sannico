b1d5f3655568ca4ed0c51fb2faa81f92fe164a12
alexesteban <alexesteban_89@hotmail.es>
2016-10-17 12:24:40 -0500

    Fix Fathers By Student Service

--- servicios/readAcudientesForStudent.php
+++ servicios/readAcudientesForStudent.php
@@ -5,7 +5,7 @@
 	$data = json_decode(file_get_contents("php://input"));
 	$guid = mysqli_real_escape_string($conn, $data->guid);
 
-	$result = $conn->query("SELECT * FROM USERS U
+	$result = $conn->query("SELECT * FROM users U
   INNER JOIN fathers_by_student FS ON U.guid = FS.guid_father
   WHERE FS.guid_student = '$guid'");
 
